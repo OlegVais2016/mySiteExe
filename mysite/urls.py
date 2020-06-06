dababbeeb30dc138a27e67a70b59f2dfd36d44c7
Oleg Vaiser <aly9681082@gmail.com>
2020-06-06 20:34:22 +0300

    correct

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,5 +24,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('mainApp/', include('mainApp.urls')),
     path('admin/', admin.site.urls),
 ]
